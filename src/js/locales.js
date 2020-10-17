678b27d63e31ec542e157fd1eb635a4de769a41a
James Duncan <james.duncan@cru.org>
2020-10-17 20:59:24 +0700

    set default language

--- src/js/locales.js
+++ src/js/locales.js
@@ -8,9 +8,9 @@ export const customLocales = [
   //Add for non iso languages - copied some from wikipedia, not sure if its correct.
   {name:"Tibetan", local:"བོད་སྐད་", location: "", tag: "bo"},
   {name:"Central Tibetan", local:"དབུས་སྐད", location: "", tag: "ct"},
-  {name:"Central Tibetan Romanized", local:"Ü-Tsang", location: "", tag: "ctr"},
+  {name:"Central Tibetan Romanized", local:"Central Tibetan", location: "", tag: "ctr"},
   {name:"Amdo Tibetan", local:"ཨ་མདོ་སྐད", location: "", tag: "at"},
-  {name:"Amdo Tibetan Romanized", local:"Amdolese", location: "", tag: "atr"},
+  {name:"Amdo Tibetan Romanized", local:"Amdo Tibetan", location: "", tag: "atr"},
 ];
 
 

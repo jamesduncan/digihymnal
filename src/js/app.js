998298507c549fc7cd31a84c24b480e67377f60a
James Duncan <james.duncan@cru.org>
2020-10-17 10:41:47 +0700

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,32 @@
+import $$ from 'dom7';
+import Framework7 from 'framework7/framework7.esm.bundle.js';
+
+// Import F7 Styles
+import 'framework7/css/framework7.bundle.css';
+
+// Import Icons and App Custom Styles
+
+import '../css/app.css';
+
+// Import Routes
+import routes from './routes.js';
+
+// Import main app component
+import App from '../app.f7.html';
+
+var app = new Framework7({
+  root: '#app', // App root element
+  component: App, // App main component
+
+  name: 'digiHymnal', // App name
+  theme: 'auto', // Automatic theme detection
+
+
+
+  // App routes
+  routes: routes,
+  // Register service worker
+  serviceWorker: {
+    path: '/service-worker.js',
+  },
+});

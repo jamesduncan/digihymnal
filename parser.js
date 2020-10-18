0808f03ab5a6c2b581f2a67da9822f43f6a9aae1
IsaacS <greenace14@gmail.com>
2020-10-18 14:21:06 +0700

    + Parser version .01 and sample files
    Uses lots of regx and java script string splitting. I have it sent to read a whole dir and convert all files in there. Requires "directory-tree" node module.

---
+++ parser.js
@@ -0,0 +1,160 @@
+const dirTree = require("directory-tree");
+var fs = require('fs');
+const { stringify } = require("querystring");
+var parseDir = function (path) {
+  // an npm module that makes a tree out of a dir
+  var currDir = dirTree(path);
+  //return currDir
+  currDir.children.forEach(element => {
+    openFile(element.path)
+  });
+};
+function openFile(path){
+  var fs = require('fs');
+  fs.readFile(path, 'ucs2', function(err, data) {
+      if (err) throw err;
+      var filename = path.split("/").pop()
+      filename = path.split("\\").pop()
+      filename = filename.split(".").shift()
+      console.log(filename)
+      onSong2JSON(data, filename);
+  });
+}
+
+// FEED ME YOUR    T E S T    D A T A
+parseDir("D:/schubert.dev/parser/samples")
+
+
+
+var testData = (`Kencho Gi Gawa
+Dzongkha
+Key: E
+
+[E]Kencho [G#m]Choe gi [A]sem gi phodrang [E]na
+nga [A]lu [E]Choe gi za[B]na
+[C#m]Choe gi [G#m]gawa gi [A]nga [E]lu
+zhing[F#m]kham gi [B]lam [E]toen ye
+[E]Ha..[G#m]lle..[A]lu…[E]jah
+Ha..[A]lle..[E]lu…[B]jah
+[C#m]Choe gi [G#m]gawa gi [A]nga [E]lu
+zhing[F#m]kham gi [B]lam [E]toen ye
+
+[E]Kencho [G#m]Choe gi [A]sem gi phodrang [E]na
+nga [A]lu [E]Choe gi za[B]na
+[C#m]Choe gi [G#m]gawa gi [A]nga [E]lu
+zhing[F#m]kham gi [B]lam [E]toen ye
+[E]Ha..[G#m]lle..[A]lu…[E]jah
+Ha..[A]lle..[E]lu…[B]jah
+[C#m]Choe gi [G#m]gawa gi [A]nga [E]lu
+zhing[F#m]kham gi [B]lam [E]toen ye`)
+// This one is guaranteed to work
+// onSong2JSON(testData)
+
+
+//this will parse XML file
+function onSong2JSON(data, filename) {
+  try {
+    var song = {};
+
+    // Clean any un-needed characters
+    function cleanString(input) {
+      //  TODO strip out all '/t'
+      var output = "";
+      var output = input.replace(/([\t])/g, '');
+      //for (var i=0; i<input.length; i++) {
+      //  if (input.charCodeAt(i) <= 127) {
+      //    output += input.charAt(i);
+      //  }
+      //}
+      return output;
+    }
+    data = cleanString(data)
+  
+
+
+    // select the first three lines
+    // [0] is the whole dataset, first line is '1'
+    var dataArray = data.split(/\r?\n/)
+    //var getMetaLines = /(.*?)\n(.*?)\n(.*?)\n(.*)/
+    // first line
+    //song.title = (getMetaLines.exec(data)[1])
+    song.title = (dataArray[0])
+    // second line
+    //song.author = (getMetaLines.exec(data)[2])
+    song.author = (dataArray[1])
+    // third line skip "Key: " 
+    var selectKey = /(?![Key: ])(.*)/
+    //console.log(selectKey.exec("Key: G#")[0])
+    //console.log(selectKey.exec("Key: C")[0])
+    //song.key = (selectKey.exec(getMetaLines.exec(data)[3])[0])
+    song.key = (selectKey.exec(dataArray[2])[0])
+
+    
+    song.lyrics = {}
+    song.lyrics.verses = []
+
+    // TODO I'm assuming data starts on 5th line
+    let lyricsStart = 4
+    // Make array of just lyrics
+    var toBeParsed = dataArray.splice(lyricsStart)
+    //console.log(lyrics)
+
+    // A temporary variable that toBeParsed calls
+    var currentVerse = []
+    // TODO use label, using a number for now
+    var currentVerseLabel = 1
+
+    // add a line to the verse
+    function addToVerse(currentLine){
+      currentVerse.push({line:currentLine})
+    }
+
+    // close the verse, push it to the song, and clear it
+    function closeVerse(){
+      label = ("verse " + stringify(currentVerseLabel))
+      song.lyrics.verses.push({verse:currentVerse})
+      currentVerseLabel++
+      currentVerse = []
+    }
+    
+    // parse each remaining line in the file
+    toBeParsed.forEach(line => {
+      if(line == ""){
+        // If new verse/chorus
+        closeVerse()
+      } else {
+        var currentLine = []
+        function addPhrase(phrase){
+          currentLine.push({phrase})
+        }
+
+        var phrases = (line.split(/\r?\[/))
+        phrases.forEach(phrase => {
+          let currentPhrase = phrase.split(/\r?]/) // try splitting
+          if(currentPhrase.length === 2){ // has chord
+            addPhrase(currentPhrase)
+          } else if (currentPhrase.length === 1 && currentPhrase != ""){ // Just lyric
+            addPhrase(['', currentPhrase[0]])
+          }
+        });
+        addToVerse(currentLine)        
+      }
+    });
+    if (currentVerse.length>0){
+      closeVerse()
+    }
+
+    // print the file
+    console.log(song)
+
+    // Output the file...
+    fs.writeFile((`./json/${filename}.json`), JSON.stringify(song), function(err) {
+      if (err) {
+          console.log(err);
+      }
+  });
+
+  } catch (e) {
+    console.error(e.message);
+  }
+}

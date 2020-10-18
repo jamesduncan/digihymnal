46677b298451620309732e1d79043c7d46a36c60
Nate <7259@pm.me>
2020-10-18 10:34:46 +0700

    add transpose function

--- src/js/transpose.js
+++ src/js/transpose.js
@@ -1,9 +1,16 @@
-//Proof of concept chord transpose. Move somewhere else.
+//Currently only support # not . Can extend in future.
 const chords = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"];
 
-function transpose (chord, steps) {
+function transposeChord (chord, steps) {
   let index = chords.indexOf(chord) + steps;
   index = index < 0 ? index + 12 :
           index > 12 ? index - 12 : index;
   return chords[index];
+}
+
+//Extracts and replaces a transposed chord from a string ex. Dm7/F# -> D#m7/G
+export function transpose (chordString, steps) {
+  return chordString.replace(/[A-F]#?/g, (chord) => {
+  	return transposeChord(chord, steps);
+   });
 }

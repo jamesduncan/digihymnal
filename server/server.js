7361bdefd3380d2ecee20b6774432ec91627faba
IsaacS <greenace14@gmail.com>
2021-07-11 17:27:06 +0700

    example server that works for saving files
    Also getting rid of ugly "unknown" file

--- server/server.js
+++ server/server.js
@@ -1,18 +1,80 @@
 const http = require('http')
+const fs = require('fs');
+
 //
 //'./test/data/song.js'
 //
 
 const server = http.createServer(function(request, response) {
+  // none of these work :(
   //console.dir(request)
-  console.dir(request.param)
-  console.log(request.headers['access-control-request-method'])
+  //console.log(request.headers['access-control-request-method'])
+  //console.dir(request.param)
+  // request.on('headers', function(headers) {
+  //   console.log({'headers':headers})
+  // })
+
   request.on('data', function(data) {
+    // this is the working logger
     console.log({'data':data.toString()})
+    try{
+      // TODO check a password
+      var usableData = JSON.parse(data.toString());
+      var password = usableData.password;
+      var songId = usableData.songId;
+      var song = usableData.data
+
+      // get filename 
+      // filename is just the title under the AT translation
+      // AT, else grab EN, else whatever
+      var filename;
+      if(song.title['at']){
+        filename = (song.title['at'])
+      } else if (song.title['en']){
+        filename = (song.title['en'])
+      }else{
+        // TODO check if this actually works
+        for (const tag in song.title) {
+          if (Object.hasOwnProperty.call(song.title, tag)) {
+            const element = song.title[tag];
+            filename = element
+            break;
+          }
+        }
+      }
+      filename = filename.concat('.js')
+
+      var songData = ('export default ').concat(JSON.stringify(song))    
+      
+      if(password === 'password'){
+      // write to file
+      fs.writeFile(`./test/${filename}`, songData, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log(`${filename} was saved!`);
+        }); 
+      } else{
+        console.log('Bad Password')
+      }
+      // rebuild??? server rebuild
+      
+
+      // TODO tell client that save worked?
+      request.on('end', function() {
+        response.end('post received')
+      })
+      // request.on('end', function() {
+      //   console.log(data)
+      //   response.writeHead(200, {'Content-Type': 'text/html'})
+      //   response.writeHead(200)
+      //   response.end('post received')
+      // })
+   }catch(e){
+     console.error(e)
+   }
   })
-  request.on('param', function(param) {
-    console.log({'param':param})
-  })
+  
 
   // if (request.method == 'POST') {
   if (request.headers['access-control-request-method'] == 'POST') {
@@ -23,7 +85,7 @@ const server = http.createServer(function(request, response) {
     })
     request.on('end', function() {
       console.log(data)
-      // response.writeHead(200, {'Content-Type': 'text/html'})
+      response.writeHead(200, {'Content-Type': 'text/html'})
       response.writeHead(200)
       response.end('post received')
     })

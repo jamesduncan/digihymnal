2bca593381f4334c2ace5af9caa371915b46bf89
Achoobert <greenace14@gmail.com>
2021-11-12 17:09:46 +0700

    Example samples

---
+++ workflow.js
@@ -0,0 +1,27 @@
+// Notes
+
+// reader
+getSongs()
+readSongs(translation)
+
+
+//musiscian 
+getSongs()
+readSongs(translation, chords, numbers)
+
+
+
+// editor
+getSongs()
+readSongs(translation, chords, numbers)
+editSong().metadata
+editSong().lyrics
+addSong() // makes empty template
+addSong(file) // ?????
+deleteSong()
+
+
+
+
+// server
+i
